e8eb733530f773df9a74c376474e9c2f8d1af312
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-06-10 21:09:33 -0400

    extract breakpoint from tailwind theme var

--- src/lib/components/breakpoint-provider.svelte
+++ src/lib/components/breakpoint-provider.svelte
@@ -3,7 +3,13 @@
 	Singleton for providing theme breakpoints in js
 -->
 <script module lang="ts">
+  import { browser } from '$app/environment';
   import { MediaQuery } from 'svelte/reactivity';
 
-  export const lg = new MediaQuery('width >= 48rem', false);
+  const styles = browser ? getComputedStyle(document.documentElement) : null;
+
+  export const lg = new MediaQuery(
+    `width >= ${styles?.getPropertyValue('--breakpoint-lg') ?? '48rem'}`,
+    false,
+  );
 </script>
