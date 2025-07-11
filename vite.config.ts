1ceb65ce8a0f6ea279eb446cd46208a3e7efdfcb
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-07-11 02:33:58 -0400

    attempt markdown collections typing using augmentation module (wip)

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,30 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { z } from 'zod/v4';
-import markdown from './plugins/markdown';
+import markdown, { defineCollections } from './plugins/markdown';
 import { uniqueItemsSchema } from './src/lib/common/schema';
 import { disciplines, tags } from './src/lib/data/meta';
 import { locales } from './src/lib/i18n/generated/runtime';
 import { extensions } from './svelte.config';
 
-function group(arr: string[] | readonly string[]) {
-  if (!arr.length) {
-    return '';
-  }
-  if (arr.length === 1) {
-    return arr[0];
-  }
-  return `{${arr.join(',')}}`;
-}
-
-const pattern = `*/${group(locales)}${group(extensions)}`;
+const collections = defineCollections({
+  works: {
+    directory: '/content/works/',
+    pattern: `*/{${locales.join(',')},}{${extensions.join(',')},}`,
+    schema: z.object({
+      year: z.number(),
+      title: z.string(),
+      shortTitle: z.string().optional(),
+      disciplines: uniqueItemsSchema(z.enum(disciplines).array()).optional(),
+      tags: uniqueItemsSchema(z.enum(tags).array()).optional(),
+      summary: z.string().optional(),
+      covers: z.string().optional().array(),
+      banner: z.string().optional(),
+      link: z.url({ protocol: /^https?$/, hostname: z.regexes.domain }),
+      code: z.url({ protocol: /^https?$/, hostname: z.regexes.domain }),
+    }),
+  },
+});
 
 export default defineConfig({
   plugins: [
@@ -31,29 +38,7 @@ export default defineConfig({
     }),
     markdown({
       extensions,
-      content: {
-        name: 'virtual:content',
-        collections: {
-          works: {
-            directory: 'content/works/',
-            pattern,
-            schema: z.object({
-              year: z.number(),
-              title: z.string(),
-              shortTitle: z.string().optional(),
-              disciplines: uniqueItemsSchema(
-                z.enum(disciplines).array(),
-              ).optional(),
-              tags: uniqueItemsSchema(z.enum(tags).array()).optional(),
-              summary: z.string().optional(),
-              covers: z.string().optional().array(),
-              banner: z.string().optional(),
-              link: z.url({ protocol: /^https?$/, hostname: z.regexes.domain }),
-              code: z.url({ protocol: /^https?$/, hostname: z.regexes.domain }),
-            }),
-          },
-        },
-      },
+      collections,
     }),
     tailwindcss(),
     enhancedImages(),
