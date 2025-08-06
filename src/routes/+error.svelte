b5e7f6ec3d9c068be204fb4a8fa91b753d30c32b
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-08-06 11:23:33 -0400

    update base error comp

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,1 +1,20 @@
-Not found 🤦
+<script>
+  import { page } from '$app/state';
+</script>
+
+<div
+  class="
+    flex h-dvh w-full flex-col items-center justify-center p-padding
+    filter-(--pixelate)
+  "
+>
+  <div class="flex max-w-prose-body flex-col items-start gap-gap">
+    <span class="text-2xl">🫢</span>
+    <h1>
+      {page.status}
+    </h1>
+    <p class="text-sm opacity-60">
+      {page.error?.message}
+    </p>
+  </div>
+</div>
