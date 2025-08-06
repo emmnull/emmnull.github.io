90a64c18a1636f47f5bf32a6e95cc2414e065b99
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-08-06 12:00:15 -0400

    fix error comp styles

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,14 +2,9 @@
   import { page } from '$app/state';
 </script>
 
-<div
-  class="
-    flex h-dvh w-full flex-col items-center justify-center p-padding
-    filter-(--pixelate)
-  "
->
-  <div class="flex max-w-prose-body flex-col items-start gap-gap">
-    <span class="text-2xl">🫢</span>
+<div class="flex h-dvh w-full flex-col items-center justify-center p-padding">
+  <div class="flex w-full max-w-prose-body flex-col items-start gap-gap">
+    <span class="text-2xl filter-(--pixelate)">🫢</span>
     <h1>
       {page.status}
     </h1>
