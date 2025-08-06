a2094b168cab886eff0409b2e2cdaf8a4ff8798f
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-08-06 10:47:00 -0400

    remove base path since using profile repo

--- svelte.config.js
+++ svelte.config.js
@@ -19,11 +19,11 @@ const config = {
     alias: {
       $messages: 'src/lib/i18n/generated/messages.js',
     },
-    paths: {
-      base: process.env.BASE_PATH
-        ? `/${process.env.BASE_PATH.replace(/^\/|\/$/g, '')}`
-        : undefined,
-    },
+    // paths: {
+    //   base: process.env.BASE_PATH
+    //     ? `/${process.env.BASE_PATH.replace(/^\/|\/$/g, '')}`
+    //     : undefined,
+    // },
   },
 };
 
