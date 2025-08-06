8684cb51d54df6183b35f6940cbb8fea8e6c98b3
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-08-06 11:14:10 -0400

    try to fix deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
-  import '@fontsource-variable/figtree';
-  // import '@fontsource-variable/fraunces';
-  import '@fontsource/dm-mono';
-  import '@fontsource/instrument-serif';
-  // import '@fontsource-variable/outfit';
-  // import '@fontsource-variable/eb-garamond';
-  // import '@fontsource-variable/spline-sans-mono';
-  // Supports weights 200-800
-  // import '@fontsource-variable/newsreader';
   import BreakpointProvider from '$lib/components/breakpoint-provider.svelte';
   import ScrollProvider from '$lib/components/scroll-provider.svelte';
   import ThemeProvider from '$lib/components/theme-provider.svelte';
+  import '@fontsource-variable/figtree';
+  import '@fontsource/dm-mono';
+  import '@fontsource/instrument-serif';
   import '../app.css';
 
   let { children } = $props();
@@ -19,11 +13,4 @@
 <ScrollProvider />
 <BreakpointProvider />
 <ThemeProvider />
-<!-- <FiltersProvider /> -->
-<!-- see https://inlang.com/m/gerre34r/library-inlang-paraglideJs/sveltekit#add-the-paraglidemiddleware-to-srchooksserverts -->
-<!-- <div class="hidden">
-  {#each locales as locale}
-    <a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
-  {/each}
-</div> -->
 {@render children()}
