6b985d039c0aaa206c05fc37b387ead02b21eb4a
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-08-06 13:42:07 -0400

    update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,13 +3,20 @@
 </script>
 
 <div class="flex h-dvh w-full flex-col items-center justify-center p-padding">
-  <div class="flex max-w-prose-body flex-col items-start gap-gap">
-    <h1 class="text-2xl font-semibold">
-      <span class="filter-(--pixelate)">🫢</span>
-      {page.status}
-    </h1>
-    <p class="text-sm opacity-60">
-      {page.error?.message}
-    </p>
+  <div class="flex max-w-prose-body items-start gap-[2rem]">
+    <span class="text-2xl filter-(--pixelate)">🫢</span>
+    <hr
+      class="
+        my-[2rem] h-auto self-stretch border-r border-dotted border-current/10
+      "
+    />
+    <div>
+      <h1 class="font-mono text-2xl font-bold">
+        {page.status}
+      </h1>
+      <p class="text-sm opacity-60">
+        {page.error?.message}
+      </p>
+    </div>
   </div>
 </div>
