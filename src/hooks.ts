b9ac75c339f7c749eb42d0e329f2a8e031f8b6a9
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2025-08-16 14:36:31 -0400

    attempt fix by using explicit route param instead of reroute hook

--- src/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { deLocalizeUrl } from '$lib/i18n/generated/runtime';
-import type { Reroute } from '@sveltejs/kit';
-
-export const reroute: Reroute = (request) => {
-  return deLocalizeUrl(request.url).pathname;
-};
